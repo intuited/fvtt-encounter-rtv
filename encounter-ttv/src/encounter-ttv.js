8088f844cbc2d919559c352d52fad5ff1f9c49a7
Ted Tibbetts <ted@circulated.ca>
2023-03-11 05:00:38 +0100

    Copied logic to new files, removed old calc code, added test value.  Works.

--- encounter-ttv/src/encounter-ttv.js
+++ encounter-ttv/src/encounter-ttv.js
@@ -1,23 +1,23 @@
-class EncounterBuilder {
+class EncounterTTV {
     static getSceneControlButtons(buttons) {
         let tokenButton = buttons.find(b => b.name === "token");
 
         if (tokenButton && game.user.isGM) {
             tokenButton.tools.push({
-                name: "encounter-builder",
+                name: "encounter-ttv",
                 title: game.i18n.localize('EB.Title'),
                 icon: "fas fa-crown",
                 visible: game.user.isGM,
-                onClick: () => EncounterBuilder.openForm()
+                onClick: () => EncounterTTV.openForm()
             });
         }
     }
 
     static openForm() {
         if (this.form === undefined) {
-            this.form = new EncounterBuilderApplication(game.actors);
+            this.form = new EncounterTTVApplication(game.actors);
         }
         this.form.render(true);
     }
 }
-Hooks.on('getSceneControlButtons', EncounterBuilder.getSceneControlButtons);
+Hooks.on('getSceneControlButtons', EncounterTTV.getSceneControlButtons);
---
+++ encounter-ttv/src/ttv-form.js
@@ -0,0 +1,242 @@
+let EB = {};
+EB.borderStyle = "2px solid rgb(120, 46, 34)";
+EB.highlightStyle = "";
+
+Handlebars.registerHelper("capitalizeAll", function (str) {
+    return str.toUpperCase();
+});
+
+class EncounterTTVApplication extends Application {
+    constructor(Actors, options = {}) {
+        super(options);
+
+        if (!game.user.isGM) return;
+
+        this.object = Actors
+        this.allies = [];
+        this.opponents = [];
+        this.test = "Initial value of test property";
+        game.users.apps.push(this)
+    }
+
+    static get defaultOptions() {
+        const options = super.defaultOptions;
+        options.title = game.i18n.localize("EB.Title");
+        options.id = game.i18n.localize("EB.id");
+        options.template = "modules/encounter-ttv/templates/ttv-app.html";
+        options.closeOnSubmit = true;
+        options.popOut = true;
+        options.width = 510;
+        options.height = "auto";
+        options.classes = ["encounter-ttv", "ttv-form"];
+        return options;
+    }
+
+    async getData() {
+        return {
+            allies: this.allies,
+            opponents: this.opponents,
+            test: this.test
+        };
+    }
+
+    activateListeners(html) {
+        super.activateListeners(html);
+        html.find("#EBContainers .actor-container").each((i, li) => {
+            li.setAttribute("draggable", true);
+            li.addEventListener("dragstart", this._onDragStart, false);
+            li.addEventListener("click", this._onClickPortrait.bind(this));
+        });
+        html.find("#EBContainers .group-container").each((i, li) => {
+            li.addEventListener("dragover", this._onDragOverHighlight);
+            li.addEventListener("dragleave", this._onDragLeaveHighlight);
+        })
+        html.find("#EBContainers .ally-container")[0].addEventListener("drop", this._onDropAlly.bind(this));
+        html.find("#EBContainers .opponent-container")[0].addEventListener("drop", this._onDropOpponent.bind(this));
+        html.find("#EBXP .clear")[0].addEventListener("click", this._onClickButton);
+        html[0].render = this.render;
+        html[0].ondragover = this._onDragOver;
+        html[0].ondrop = this._onDrop;
+    }
+
+    /**
+     * Performs calculations determine TTV for both sides of the encounter.
+     *
+     * @memberof EncounterBuilderApplication
+     */
+    calc() {
+        // TODO
+    }
+
+    /**
+     * Ondrop template for ally and opponent fields. Attempts to return builder Application and Actor of interest.
+     *
+     * @param {*} event
+     * @returns {Array}
+     * @memberof EncounterBuilderApplication
+     */
+    async _onDropGeneral(event) {
+        const data = JSON.parse(event.dataTransfer.getData("text/plain"));
+        const actors = []
+
+        function recur_folder(folder) {
+            const actors = folder.contents
+            const subfolders = folder.getSubfolders()
+            for (let i = 0; i < subfolders.length; i++) {
+                actors.push(...recur_folder(subfolders[i]))
+            }
+
+            return actors
+        }
+        if (data.type === game.folders.documentName && data.documentName === game.actors.documentName) {
+            const folder = await Folder.fromDropData(data)
+            actors.push(...recur_folder(folder))
+        }
+        else if (data.type === game.actors.documentName) {
+            const actor = await Actor.fromDropData(data);
+            actors.push(actor)
+        }
+
+        else {
+                throw new Error(game.i18n.localize("EB.EntityError"));
+        }
+
+        const app = game.users.apps.find(e => e.id === game.i18n.localize("EB.id"));
+        return [app, actors]
+    }
+
+    /**
+     * Ondrop for allies. Cannot have a playable character multiple times. Can have monsters/npcs multiple times.
+     *
+     * @param {*} event
+     * @memberof EncounterBuilderApplication
+     */
+    async _onDropAlly(event) {
+        event.preventDefault();
+        let [app, actors] = await this._onDropGeneral(event);
+        await this.processDrop(event, app.allies, app.opponents, app, actors)
+    }
+
+
+    /**
+     * Ondrop for opponents. Cannot have a playable character multiple times. Can have monsters/npcs multiple times.
+     *
+     * @param {*} event
+     * @memberof EncounterBuilderApplication
+     */
+    async _onDropOpponent(event) {
+        event.preventDefault();
+        let [app, actors] = await this._onDropGeneral(event);
+        await this.processDrop(event, app.opponents, app.allies, app, actors)
+    }
+
+    async processDrop(event, currentDropZone, opposingDropZone, app, actors) {
+
+        let actorExists;
+        let actorExistsOpposing;
+        actors.forEach(function (actor) {
+            if (actor.type === "character") {
+                actorExists = currentDropZone.find(e => e.id === actor.id)
+                actorExistsOpposing = opposingDropZone.find(e => e.id === actor.id);
+
+                if (actorExistsOpposing) {
+                    let ix = opposingDropZone.findIndex(e => e.id === actor.id);
+                    opposingDropZone.splice(ix, 1);
+                }
+                if (!actorExists) {
+                    currentDropZone.push(actor)
+                }
+            }
+            else if (actor.type === "npc") {
+                currentDropZone.push(actor);
+            }
+        })
+
+        app.calc();
+        app.render();
+    }
+
+    _onDragOverHighlight(event) {
+        const li = this;
+        li.style["border"] = EB.borderStyle;
+        li.style["background"] = EB.highlightStyle;
+    }
+
+    _onDragLeaveHighlight(event) {
+        const li = this;
+        li.style["border"] = "";
+        li.style["background"] = "";
+    }
+
+    /**
+     * Ondragstart for character portraits, sets data necessary to drag to canvas.
+     *
+     * @param {*} event
+     * @memberof EncounterBuilderApplication
+     */
+    _onDragStart(event) {
+        event.stopPropagation();
+        const id = this.firstElementChild.id
+        const actor = game.actors.get(id)
+
+        event.dataTransfer.setData("text/plain", JSON.stringify({
+            type: game.actors.documentName,
+            uuid: actor.uuid
+        }));
+    }
+
+    /**
+     * Remove actor from calculation on clicking the portrait.
+     *
+     * @param {*} event
+     * @memberof EncounterBuilderApplication
+     */
+    _onClickPortrait(event) {
+        event.stopPropagation();
+
+        const srcClass = event.srcElement.classList.value;
+        const isPortrait = srcClass === "actor-portrait";
+        const isHoverIcon = (srcClass === "actor-subtract") || (srcClass === "fas fa-minus");
+        if ((isPortrait) || (isHoverIcon)) {
+            const app = game.users.apps.find(e => e.id === game.i18n.localize("EB.id"));
+            let name = event.srcElement.title;
+            let actorExists;
+
+            const parentClass = event.srcElement.parentElement.parentElement.classList.value;
+            const parentParentClass = event.srcElement.parentElement.parentElement.parentElement.classList.value;
+            if ((parentClass === "group-field ally-field") || (parentParentClass === "group-field ally-field")) {
+                let actorExists = this.allies.find(e => e.name === name);
+                if (actorExists) {
+                    let ix = this.allies.findIndex(e => e.name === name);
+                    this.allies.splice(ix, 1);
+                }
+            }
+            else if ((parentClass === "group-field opponent-field") || (parentParentClass === "group-field opponent-field")) {
+                let actorExists = this.opponents.find(e => e.name === name);
+                if (actorExists) {
+                    let ix = this.opponents.findIndex(e => e.name === name);
+                    this.opponents.splice(ix, 1);
+                }
+            }
+            app.calc();
+            app.render();
+        }
+    }
+
+    /**
+     * Clears list of allies and opponents.
+     *
+     * @param {*} event
+     * @memberof EncounterBuilderApplication
+     */
+    _onClickButton(event) {
+        event.stopPropagation();
+        const app = game.users.apps.find(e => e.id === game.i18n.localize("EB.id"));
+        app.allies = [];
+        app.opponents = [];
+
+        app.calc();
+        app.render();
+    }
+
+}
